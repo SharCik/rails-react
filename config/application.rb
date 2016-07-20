fde58e74f01583fcd3d116ec16d1a53d55d393d0
Stas Zelenko <sharcik14@gmail.com>
2016-07-20 12:52:27 +0300

    create component PeopleCard

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module RailsReact
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # React config
+    config.react.addons = true
+
   end
 end
